baabd82828a37c7e6d38cd63cb8cf3ed3686b7bb
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2024-05-09 15:35:59 +0800

    update

--- src/Pixer/Pixelate/views.py
+++ src/Pixer/Pixelate/views.py
@@ -5,6 +5,7 @@ import cv2
 
 from Pixelate.ImageProcesser import ImageProcesser
 from libs.fake_data import Fake
+from PIL import Image
 
 # Create your views here.
 def index(request):
@@ -16,25 +17,34 @@ def test(request):
     img_data = Fake.get_byte_image()
     img_buf, size = img_data["image"], img_data["size"]
     img_ary = ip.byte_to_image(img_buf, (size[1], size[0], 3))
-    
-    img_res = ip.process(img_ary, 24, 'rgb', 8)
+    scale = 32
     
     cv2.imshow("Original", img_ary[:, :, ::-1])
     cv2.waitKey(0)
     
+    img_res = ip.process(img_ary, 24, 'rgb', scale)
     cv2.imshow("24-bit pixel", img_res[:, :, ::-1])
     cv2.waitKey(0)
+    img = Image.fromarray(img_res)
+    img.save("./static/imgs/test/24-bit.png")
     
-    img_res = ip.process(img_ary, 12, 'rgb', 8)
+    img_res = ip.process(img_ary, 12, 'rgb', scale)
     cv2.imshow("12-bit pixel", img_res[:, :, ::-1])
     cv2.waitKey(0)
+    img = Image.fromarray(img_res)
+    img.save("./static/imgs/test/12-bit.png")
     
-    img_res = ip.process(img_ary, 6, 'rgb', 8)
+    
+    img_res = ip.process(img_ary, 6, 'rgb', scale)
     cv2.imshow("6-bit pixel", img_res[:, :, ::-1])
     cv2.waitKey(0)
+    img = Image.fromarray(img_res)
+    img.save("./static/imgs/test/6-bit.png")
     
-    img_res = ip.process(img_ary, 3, 'rgb', 8)
+    img_res = ip.process(img_ary, 3, 'rgb', scale)
     cv2.imshow("3-bit pixel", img_res[:, :, ::-1])
     cv2.waitKey(0)
+    img = Image.fromarray(img_res)
+    img.save("./static/imgs/test/3-bit.png")
     
     return HttpResponse("test")
--- src/Pixer/libs/fake_data.py
+++ src/Pixer/libs/fake_data.py
@@ -3,7 +3,7 @@ from PIL import Image
 class Fake:
 
     def get_byte_image() -> bytes:
-        img = Image.open(r"C:\Users\bfkam\Downloads\GM1X0CSboAArOs2.jpg")
+        img = Image.open(r"D:\OneDrive\圖片\桌布\ancient-pagoda-stands-tall-autumn-forest-generated-by-ai2.jpg")
         img_format = img.format
         img = img.convert("RGB")
         size = img.size
