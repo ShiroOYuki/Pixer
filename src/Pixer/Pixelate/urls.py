36168432aa6525ced8f9c242cbfc6d424a2b4840
uiskskkekekk <angus.lu0611@gmail.com>
2024-06-08 22:53:04 +0800

    Update urls.py

--- src/Pixer/Pixelate/urls.py
+++ src/Pixer/Pixelate/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name="pixelate"),
     path('upload', views.process_image, name="process_image"),
     path('test', views.test, name="test"),
-    path('pixelate_page', views.pixelate, name="pixelate_page")
+    path('pixelate_page', views.pixelate, name="pixelate_page"),
     path('upload-gallery', views.upload_gallery, name="upload_to_gallery")
 ]
